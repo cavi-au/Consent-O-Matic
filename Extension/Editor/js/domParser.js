c8b9e3ed3b09c99b3419b901c5f45be0fe549292
Rolf Bagge <rolf@cavi.au.dk>
2020-02-06 10:01:22 +0100

    Changed jsonParser to be flexible!

--- Extension/Editor/js/domParser.js
+++ Extension/Editor/js/domParser.js
@@ -240,7 +240,7 @@ class DomParser {
     static async parseSlideActionDom(dom, result) {
         Object.assign(result, await DomParser.parseDomSelectorDom(dom.querySelector(":scope > [data-bind='target']").querySelector(":scope > [data-type='domSelector']")));
         result.dragTarget = await DomParser.parseDomSelectorDom(dom.querySelector(":scope > [data-bind='dragTarget']").querySelector(":scope > [data-type='domSelector']"));
-        let axis = dom.querySelector("[data-bind='axis']");
+        let axis = dom.querySelector("[data-bind='axis'] > select");
         result.axis = axis.value;
     }
 
@@ -298,7 +298,7 @@ class DomParser {
     static async parseConsentDom(dom) {
         let result = {};
 
-        result.type = dom.querySelector(":scope > .type").querySelector(":scope > [data-bind='type']").value;
+        result.type = dom.querySelector(":scope > [data-bind='type'] > select").value;
 
         try {
             let matcherDom = dom.querySelector(":scope > [data-bind='matcher']").querySelector(":scope > [data-type='matcher']");
--- Extension/Editor/js/jsonParser.js
+++ Extension/Editor/js/jsonParser.js
@@ -1,275 +1,90 @@
 class JsonParser {
     static async parseCmp(json) {
-        let result = await loadTemplate("cmp");
-
-        for(let detector of json.detectors) {
-            let detectorDom = await JsonParser.parseDetector(detector);
-            result.querySelector("[data-plug='detector']").appendChild(detectorDom);
-        }
-
-        for(let method of json.methods) {
-            let methodDom = await JsonParser.parseMethod(method);
-            result.querySelector("[data-plug='method']").appendChild(methodDom);
-        }
-
-        return result;
+        return await JsonParser.loadTemplate("cmp", json);
     }
 
-    static async parseMethod(json) {
-        let result = await loadTemplate("method");
+    static async loadTemplate(tplName, json) {
+        console.log("Loading template:", tplName, json);
 
-        result.querySelector(":scope > [data-bind='name']").textContent = json.name;
+        let tpl = await loadTemplate(tplName);
 
-        if(json.action != null) {
-            let actionDom = await JsonParser.parseAction(json.action);
-            result.querySelector(":scope > [data-bind='action']").appendChild(actionDom);
+        let plugs = Array.from(tpl.querySelectorAll("[data-plug]"));
+        let valueBinds = Array.from(tpl.querySelectorAll("[data-bind]:not([data-plug])"));
+
+        for (let plug of plugs) {
+            let plugType = plug.getAttribute("data-plug");
+            let bindName = plug.getAttribute("data-bind");
+
+            await JsonParser.loadPlug(plug, plugType, bindName, json);
         }
 
-        return result;
-    }
-
-    static async parseDetector(json) {
-        let result = await loadTemplate("detector");
-
-        if(json.presentMatcher != null) {
-            let presentMatcherDom = await JsonParser.parseMatcher(json.presentMatcher);
-            result.querySelector(":scope > [data-bind='presentMatcher']").appendChild(presentMatcherDom);
-        }
-        if(json.showingMatcher != null) {
-            let showingMatcherDom = await JsonParser.parseMatcher(json.showingMatcher);
-            result.querySelector(":scope > [data-bind='showingMatcher']").appendChild(showingMatcherDom);
+        for(let valueBind of valueBinds) {
+            let bindName = valueBind.getAttribute("data-bind");
+            await JsonParser.loadBind(valueBind, bindName, json);
         }
 
-        return result;
+        return tpl;
     }
 
-    static async parseMatcher(json) {
-        let result = await loadTemplate("matcher_"+json.type);
+    static async loadBind(valueBind, bindName, json) {
+        let bindJson = json[bindName];
 
-        let domSelectorDom = await JsonParser.parseDomSelection(json);
-        result.querySelector(":scope > [data-plug='domSelector']").appendChild(domSelectorDom);
+        if(bindJson != null) {
+            console.log("Value bind: ", valueBind, bindName, json);
 
-        return result;
-    }
-
-    static async parseDomSelection(json) {
-        let result = await loadTemplate("domSelector");
-
-        async function setupDomSelector(dom, json) {
-            if(json.selector) {
-                dom.querySelector("[data-bind='selector'] input").value = json.selector;
-            }
-            if(json.textFilter) {
-                let text = json.textFilter;
-                if(json.textFilter instanceof Array) {
-                    text = json.textFilter.join("|");
+            if(valueBind.querySelector("input")) {
+                //Setup value inside input
+                let input = valueBind.querySelector("input");
+                switch(input.getAttribute("type")) {
+                    case "checkbox": {
+                        input.checked = bindJson;
+                        break;
+                    }
+                    default: {
+                        input.value = bindJson;
+                    }
                 }
-                dom.querySelector("[data-bind='textFilter'] input").value = text;
-            }
-            if(json.iframeFilter) {
-                dom.querySelector("[data-bind='iframeFilter'] input").checked = json.iframeFilter;
-            }
-            if(json.displayFilter) {
-                dom.querySelector("[data-bind='displayFilter'] input").checked = json.displayFilter;
-            }
-            if(json.childFilter) {
-                let childFilterDom = await JsonParser.parseDomSelection(json.childFilter);
-                dom.querySelector(":scope > [data-bind='childFilter']").appendChild(childFilterDom);
+            } else if(valueBind.querySelector("select")) {
+                //Setup value inside select
+                let select = valueBind.querySelector("select");
+                select.value = bindJson;
+            } else {
+                //Setup value as string
+                valueBind.textContent = bindJson;
             }
         }
-
-        if(json.target) {
-            await setupDomSelector(result.querySelector(":scope > .target"), json.target);
-            result.querySelector(":scope > .target").classList.remove("toggled");
-        }
-
-        if(json.parent) {
-            await setupDomSelector(result.querySelector(":scope > .parent"), json.parent);
-            result.querySelector(":scope > .parent").classList.remove("toggled");
-        }
-
-        return result;
     }
 
-    static async parseAction(json) {
-        switch(json.type) {
-            case "list":
-                return await JsonParser.parseListAction(json);
-            case "click":
-                return await JsonParser.parseClickAction(json);
-            case "consent":
-                return await JsonParser.parseConsentAction(json);
-            case "foreach":
-                return await JsonParser.parseForEachAction(json);
-            case "ifcss":
-                    return await JsonParser.parseIfCssAction(json);
-            case "waitcss":
-                return await JsonParser.parseWaitCssAction(json);
-            case "slide":
-                return await JsonParser.parseSlideAction(json);
-            case "close":
-                return await JsonParser.parseCloseAction(json);
-            case "hide":
-                return await JsonParser.parseHideAction(json);
-            case "wait":
-                return await JsonParser.parseWaitAction(json);
-            default:
-                console.log("Unknown action type:", json.type);
+    static async loadPlug(plug, plugType, bindName, json) {
+        let plugJson = json;
+
+        if (bindName != null) {
+            plugJson = json[bindName];
         }
 
-        return document.createElement("div");
-    }
+        console.log("Loading plug:", plugType, bindName, plugJson, json);
+        if (plugJson != null) {
+            if (!(plugJson instanceof Array)) {
+                plugJson = [plugJson];
+            }
 
-    static async parseListAction(json) {
-        let result = await loadTemplate("action_list");
+            for (let plugJsonEntry of plugJson) {
+                let tplName = plugType;
+                if (plugJsonEntry.type != null && tplName !== "domSelector" && tplName !== "consent") {
+                    tplName += "_" + plugJsonEntry.type;
+                }
+    
+                let tpl = await JsonParser.loadTemplate(tplName, plugJsonEntry);
+                plug.appendChild(tpl);
+            }
+        } else {
+            console.log("DEBUG null encountered loading plug:", plugType, bindName, json);
 
-        for(let action of json.actions) {
-            let actionDom = await JsonParser.parseAction(action);
-
-            result.querySelector(":scope > [data-bind='actions']").appendChild(actionDom);
+            if(plugType === "domSelectorChild") {
+                //Insert empty anyways
+                let tpl = await JsonParser.loadTemplate("domSelectorChild", {});
+                plug.appendChild(tpl);
+            }
         }
-
-        return result;
-    }
-
-    static async parseCloseAction(json) {
-        let result = await loadTemplate("action_close");
-
-        return result;
-    }
-
-    static async parseClickAction(json) {
-        let result = await loadTemplate("action_click");
-
-        let domSelectorDom = await JsonParser.parseDomSelection(json);
-        result.querySelector(":scope > [data-plug='domSelector']").appendChild(domSelectorDom);
-
-        if(json.openInTab) {
-            result.querySelector("[data-bind='openInTab'] input").checked = json.openInTab;
-        }
-
-        return result;
-    }
-
-    static async parseHideAction(json) {
-        let result = await loadTemplate("action_hide");
-
-        let domSelectorDom = await JsonParser.parseDomSelection(json);
-        result.querySelector(":scope > [data-plug='domSelector']").appendChild(domSelectorDom);
-
-        return result;
-    }
-
-    static async parseSlideAction(json) {
-        let result = await loadTemplate("action_slide");
-
-        let domSelectorDom = await JsonParser.parseDomSelection(json);
-        result.querySelector(":scope > [data-bind='target']").appendChild(domSelectorDom);
-
-        let dragTargetDom = await JsonParser.parseDomSelection(json.dragTarget);
-        result.querySelector(":scope > [data-bind='dragTarget']").appendChild(dragTargetDom);
-
-        result.querySelector("[data-bind='axis']").value = json.axis;
-
-        return result;
-    }
-
-    static async parseWaitCssAction(json) {
-        let result = await loadTemplate("action_waitcss");
-
-        let domSelectorDom = await JsonParser.parseDomSelection(json);
-        result.querySelector(":scope > [data-plug='domSelector']").appendChild(domSelectorDom);
-
-        if(json.retries) {
-            result.querySelector("[data-bind='retries'] input").value = json.retries;
-        }
-
-        if(json.waitTime) {
-            result.querySelector("[data-bind='waitTime'] input").value = json.waitTime;
-        }
-
-        if(json.negated) {
-            result.querySelector("[data-bind='negated'] input").checked = json.negated;
-        }
-
-        return result;
-    }
-
-    static async parseWaitAction(json) {
-        let result = await loadTemplate("action_wait");
-
-        if(json.waitTime) {
-            result.querySelector("[data-bind='waitTime'] input").value = json.waitTime;
-        }
-        
-        return result;
-    }
-
-    static async parseConsentAction(json) {
-        let result = await loadTemplate("action_consent");
-
-        for(let consent of json.consents) {
-            let consentDom = await JsonParser.parseConsent(consent);
-
-            result.querySelector(":scope > [data-bind='consents']").appendChild(consentDom);
-        }
-
-        return result;
-    }
-
-    static async parseConsent(json) {
-        let result = await loadTemplate("consent");
-
-        result.querySelector("[data-bind='type']").value = json.type;
-
-        if(json.matcher) {
-            let matcherDom = await JsonParser.parseMatcher(json.matcher);
-            result.querySelector(":scope > [data-bind='matcher']").appendChild(matcherDom);
-        }
-
-        if(json.toggleAction) {
-            let toggleActionDom = await JsonParser.parseAction(json.toggleAction);
-            result.querySelector(":scope > [data-bind='toggleAction']").appendChild(toggleActionDom);
-        }
-        if(json.trueAction) {
-            let trueActionDom = await JsonParser.parseAction(json.trueAction);
-            result.querySelector(":scope > [data-bind='trueAction']").appendChild(trueActionDom);
-        }
-        if(json.falseAction) {
-            let falseActionDom = await JsonParser.parseAction(json.falseAction);
-            result.querySelector(":scope > [data-bind='falseAction']").appendChild(falseActionDom);
-        }
-
-        return result;
-    }
-
-    static async parseForEachAction(json) {
-        let result = await loadTemplate("action_foreach");
-
-        let domSelectorDom = await JsonParser.parseDomSelection(json);
-        result.querySelector(":scope > [data-plug='domSelector']").appendChild(domSelectorDom);
-
-        let actionDom = await JsonParser.parseAction(json.action);
-        result.querySelector(":scope > [data-bind='action']").appendChild(actionDom);
-
-        return result;
-    }
-
-    static async parseIfCssAction(json) {
-        let result = await loadTemplate("action_ifcss");
-
-        let domSelectorDom = await JsonParser.parseDomSelection(json);
-        result.querySelector(":scope > [data-plug='domSelector']").appendChild(domSelectorDom);
-
-        if(json.trueAction) {
-            let trueActionDom = await JsonParser.parseAction(json.trueAction);
-            result.querySelector(":scope > [data-bind='trueAction']").appendChild(trueActionDom);
-        }
-        if(json.falseAction) {
-            let falseActionDom = await JsonParser.parseAction(json.falseAction);
-            result.querySelector(":scope > [data-bind='falseAction']").appendChild(falseActionDom);
-        }
-
-        return result;
     }
 }
