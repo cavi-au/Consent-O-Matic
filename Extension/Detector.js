f574658ad348d46d556a46639986bdbb5cb105db
Rolf Bagge <rolf@cavi.au.dk>
2022-04-07 14:40:17 +0200

    Allow multiple Matchers on a detector. The detector only triggers if all matchers match

--- Extension/Detector.js
+++ Extension/Detector.js
@@ -2,15 +2,33 @@ class Detector {
     constructor(config) {
         this.config = config;
 
-        this.presentMatcher = Matcher.createMatcher(this.config.presentMatcher);
-        this.showingMatcher = Matcher.createMatcher(this.config.showingMatcher);
+        this.presentMatchers = [];
+        this.showingMatchers = [];
+
+        if(!Array.isArray(this.config.presentMatcher)) {
+            this.config.presentMatcher = [this.config.presentMatcher];
+        }
+        if(!Array.isArray(this.config.showingMatcher)) {
+            this.config.showingMatcher = [this.config.showingMatcher];
+        }
+
+        this.config.presentMatcher.forEach((matcher)=>{
+            this.presentMatchers.push(Matcher.createMatcher(matcher));
+        });
+        this.config.showingMatcher.forEach((matcher)=>{
+            this.showingMatchers.push(Matcher.createMatcher(matcher));
+        });
     }
 
     detect() {
-        return this.presentMatcher.matches();
+        return this.presentMatchers.every((matcher)=>{
+            return matcher.matches();
+        });
     }
 
     isShowing() {
-        return this.showingMatcher.matches();
+        return this.showingMatchers.every((matcher)=>{
+            return matcher.matches();
+        });
     }
 }
